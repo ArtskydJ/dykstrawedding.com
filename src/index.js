5aba98d8e6103746dd2c03425fa82a9e64b20d78
Joseph Dykstra <josephdykstra@gmail.com>
2019-10-15 08:58:37 -0500

    Remove bundling, and add all my siblings (#5)
    * Add all my siblings to index.html
    * Create countdown@2.6.0.js
    * Add all my siblings to index.js and use window.countdown
    * Delete bundle.js
    * Delete package.json
    * Delete .gitignore
    * Use src files instead of bundled files in index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-var countdown = require('countdown')
-
 function makeCountdown(selector, dateStr, happyCouple) {
 	var ele = document.querySelector(selector)
 
@@ -9,8 +7,15 @@ function makeCountdown(selector, dateStr, happyCouple) {
 			'Congratulations ' + happyCouple + '!!!'
 	}
 
-	countdown(onCount, new Date(dateStr))
+	window.countdown(onCount, new Date(dateStr))
 }
 
+makeCountdown('#jordon-jessica', '2008 August 16 12:00 cst', 'Jordon and Jessica')
 makeCountdown('#seth-laura', '2015 May 16 12:00 cst', 'Seth and Laura')
+makeCountdown('#david-ruth', '2012 April 20 12:00 cst', 'David and Ruth')
 makeCountdown('#michael-elizabeth', '2015 November 20 12:00 cst', 'Michael and Elizabeth')
+makeCountdown('#micah-rebecca', '2016 December 17 12:00 cst', 'Micah and Rebecca')
+// makeCountdown('#joseph-nooneyetlol', 'Future', 'Joseph and wifey')
+makeCountdown('#levi-hannah', '2019 November 11 12:00 cst', 'Levi and Hannah')
+// makeCountdown('#daniel-nooneyet', 'Future', 'Daniel and wifey')
+// makeCountdown('#nathanael-noone', 'Future', 'Nathanael and wifey')
